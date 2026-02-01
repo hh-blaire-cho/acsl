ed3d5e4559c02dc7dc08e05d5cf8a8384041ff32
Blaire <hh.blaire.cho@gmail.com>
2026-02-01 15:44:35 +0900

    renaming chmod 4 methods and formatting

--- swea/c1/chmod_4.py
+++ swea/c1/chmod_4.py
@@ -4,29 +4,30 @@ sys.stdin = open("./inputs/in_chmod_4.txt", "r")
 input = sys.stdin.readline
 
 
-def binary2rw(code, p):
+def binary2rwx(code):
     ret = list("rwx")
     for i in range(3):
         if code[i] == "0":
             ret[i] = "-"
-    return ret
+    return "".join(ret)
 
 
 def solve():
-    p, x, y, z = map(int, input().split())  # special permission and three octal numbers
+    p, x, y, z = map(int, input().split())
     code1 = bin(x)[2:].zfill(3)
     code2 = bin(y)[2:].zfill(3)
     code3 = bin(z)[2:].zfill(3)
-    lst1 = binary2rw(code1, p)
-    lst2 = binary2rw(code2, p)
-    lst3 = binary2rw(code3, p)
+    lst1 = binary2rwx(code1)
+    lst2 = binary2rwx(code2)
+    lst3 = binary2rwx(code3)
 
     if p == 1 and lst1[-1] != "-":
         lst1[-1] = "s"
-    if p == 2 and lst2[-1] != "-":
+    elif p == 2 and lst2[-1] != "-":
         lst2[-1] = "s"
-    if p == 4 and lst3[-1] != "-":
+    elif p == 4 and lst3[-1] != "-":
         lst3[-1] = "t"
+
     print(
         f"{code1} {code2} {code3} and {''.join(lst1)} {''.join(lst2)} {''.join(lst3)}"
     )
